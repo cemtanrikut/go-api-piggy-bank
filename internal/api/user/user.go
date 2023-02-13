c9e838a2a8d1f258561c8f64362247f8435df8d6
Cem Tanrikut <cemtanrikut@gmail.com>
2023-02-12 21:01:47 -0800

    Add CRUD methods for user

---
+++ internal/api/user/user.go
@@ -0,0 +1,26 @@
+package api
+
+// Create an user
+func Create() {
+
+}
+
+// Get an user by userID
+func Get(userID string) {
+
+}
+
+// Get user list
+func GetList() {
+
+}
+
+// Update user
+func Update() {
+
+}
+
+// Delete user by userID
+func Delete(userID string) {
+
+}
