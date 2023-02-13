2e50e564eea69d499b0022fb7fdb888984eba792
Cem Tanrikut <cemtanrikut@gmail.com>
2023-02-12 22:16:45 -0800

    Update response helper data

--- internal/helper/helper.go
+++ internal/helper/helper.go
@@ -4,14 +4,16 @@ type ResponseModel struct {
 	StatusCode string
 	Message    string
 	Error      string
+	Data       interface{}
 }
 
-func SetResponse(statusCode, message, error string) ResponseModel {
-	data := ResponseModel{
+func SetResponse(statusCode, message, error string, data interface{}) ResponseModel {
+	respData := ResponseModel{
 		StatusCode: statusCode,
 		Message:    message,
 		Error:      error,
+		Data:       data,
 	}
 
-	return data
+	return respData
 }
