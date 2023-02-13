5be5dbd0d3f822ce68020c342e9e3b5c3e1304a4
Cem Tanrikut <cemtanrikut@gmail.com>
2023-02-12 22:05:13 -0800

    Add functions for debt and payment

--- internal/api/debt/debt.go
+++ internal/api/debt/debt.go
@@ -17,3 +17,43 @@ type paymentModel struct {
 	DebtID string
 	Date   string
 }
+
+// Add debt
+func Add(debt DebtModel) {
+
+}
+
+// Get debt by debtID
+func Get(debtID string) {
+
+}
+
+// Update debt
+func Update(debt DebtModel) {
+
+}
+
+// Delete debt
+func Delete(debtID string) {
+
+}
+
+// Add payment
+func AddPayment(payment paymentModel) {
+
+}
+
+// Get payment by payment ID
+func GetPayment(paymentID string) {
+
+}
+
+// Get Debt's payments
+func GetPaymentByDebt(debtID string) {
+
+}
+
+// Delete payment
+func DeletePayment(debtID, paymentID string) {
+
+}
